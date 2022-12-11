c9dba5b3869d1893dfd29f756beef13dca0a99bf
Костя <kroshavr@mail.ru>
2022-12-11 18:25:53 +0500

    На проверку

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,15 @@ public class Main {
         String firstName = "Ivan";
         String midlleName = "Ivanovich";
         String lastName = "Ivanov";
-
-
+        String fullName = lastName + ' ' + firstName + ' ' + midlleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        //Задание 2
+        String fullName2 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета -" + fullName2);
+        //Задание 3
+        StringBuilder sb = new StringBuilder(fullName);
+        fullName = fullName.replace("Ivanov Ivan Ivanovich", "Иванов Семён Семёнович");
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullName);
     }
 }
